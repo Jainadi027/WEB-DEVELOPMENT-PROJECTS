0d0f27ff7b473e22e25ac77e8bbe2f2725f8e68c
Aditya Jain <114854603+Jainadi027@users.noreply.github.com>
2024-05-14 15:52:10 +0530

    Add files via upload

---
+++ QR CODE GENERATOR/script.js
@@ -0,0 +1,23 @@
+// script.js
+
+const qrCodeContainer = document.getElementById("qr-code-container");
+const qrCodeImg = document.getElementById("qr-code");
+const generateBtn = document.getElementById("generate-btn");
+const dataInput = document.getElementById("data");
+
+generateBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const data = dataInput.value.trim();
+  if (data) {
+    generateQRCode(data);
+  } else {
+    alert("Please enter some data.");
+  }
+});
+
+function generateQRCode(data) {
+  const qrCodeUrl = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${data}`;
+  console.log(qrCodeUrl);
+  qrCodeImg.src = qrCodeUrl;
+  qrCodeContainer.style.display = "block";
+}
